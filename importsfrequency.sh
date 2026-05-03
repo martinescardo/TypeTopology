1eb5678d544fbaffbd4cc2c99ef4e09125ede3ae
Martin Escardo <m.escardo@cs.bham.ac.uk>
2026-05-03 13:48:24 +0100

    script that produces a frequency table of open imports

--- importsfrequency.sh
+++ importsfrequency.sh
@@ -1,5 +1,13 @@
 # Frequency table of `open imports`
 
-grep "open import" `git ls-files '*agda'` --no-filename | sed 's/^[ \t]*//' | sort | uniq -c | sort -r
+grep "open import" `git ls-files '*agda'` --no-filename \
+    | sed 's/^[ \t]*//'                                 \
+    | sed 's/open import //'                            \
+    | sed 's/ .*//'                                     \
+    | sort                                              \
+    | uniq -c                                           \
+    | sort -r
 
-# sed is used to remove all spaces at the beginning of a line
+# sed (1) removes all spaces at the beginning of a line.
+# sed (2) removes "open import ".
+# sed (3) removes the first space " " and everything that follows it.
